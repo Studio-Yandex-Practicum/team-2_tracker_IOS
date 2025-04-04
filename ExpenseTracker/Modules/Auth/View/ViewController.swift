88fb9b5d601bc2b7b2c87047a0661aa40acedb49
Karina <barakhaeva@inbox.ru>
2025-04-04 22:52:15 +0300

    Задана стурктура проекта

---
+++ ExpenseTracker/Modules/Auth/View/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var images: [String]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
