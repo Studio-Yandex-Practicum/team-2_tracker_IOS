411696d6797f784e4f5f341c0db5bb5a4f84a40e
Karina <barakhaeva@inbox.ru>
2025-04-04 16:11:16 +0300

    Инициализация проекта.  Closes #13

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by /karisheesh on 04.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
