036b6ac3ed411bab36a6bf5a684b5ec6c58f0fd5
Karina <barakhaeva@inbox.ru>
2025-04-06 22:32:44 +0300

    Установлено свойство координатора

---
+++ ExpenseTracker/Modules/Analytics/View/AnalyticsViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class AnalyticsViewController: UIViewController {
+    
+    weak var coordinator: AnalyticsCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
