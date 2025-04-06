0f76b87779b858428a5a57f74005c82cd98310cb
Karina <barakhaeva@inbox.ru>
2025-04-06 22:20:06 +0300

    Установлен корневой контроллер для window

--- ExpenseTracker/Application/SceneDelegate.swift
+++ ExpenseTracker/Application/SceneDelegate.swift
@@ -3,11 +3,17 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    private var appCoordinator: AppCoordinator?
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+     
+        UserDefaults.isLoggedIn = true
+        
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+            
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator?.start()
     }
 }
