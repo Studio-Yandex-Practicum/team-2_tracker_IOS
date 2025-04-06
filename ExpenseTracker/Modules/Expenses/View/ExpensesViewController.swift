3585675f3f961b1d862d33104baf06adee8e5c0c
Karina <barakhaeva@inbox.ru>
2025-04-06 22:39:29 +0300

    Установлено свойство координатора для ExpenseViewController

---
+++ ExpenseTracker/Modules/Expenses/View/ExpensesViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ExpensesViewController: UIViewController {
+    
+    weak var coordinator: ExpensesCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
