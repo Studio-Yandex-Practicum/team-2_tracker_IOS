936c90e07798186fdf6702db521d5542fced8956
Karina <barakhaeva@inbox.ru>
2025-04-06 22:37:35 +0300

    Добавлен координатор для экрана расходов

---
+++ ExpenseTracker/Modules/Expenses/Coordinator/ExpensesCoordinator.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+// Координатор для расходов
+final class ExpensesCoordinator: Coordinator {
+
+    // MARK: - Coordinator Properties
+    
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    
+    // MARK: - Initialization
+    
+    init() {
+        self.navigationController = UINavigationController()
+        configureTabBarItem()
+    }
+    
+    // MARK: - Coordinator Lifecycle
+    
+    func start() {
+        let expensesViewController = ExpensesViewController()
+        expensesViewController.coordinator = self
+        navigationController.setViewControllers([expensesViewController], animated: true)
+    }
+}
+
+extension ExpensesCoordinator: TabCoordinator {
+    var tabItem: TabItem { .expenses }
+    
+    private func configureTabBarItem() {
+        navigationController.tabBarItem = UITabBarItem(
+            title: tabItem.title,
+            image: tabItem.icon,
+            tag: 0
+        )
+    }
+}
