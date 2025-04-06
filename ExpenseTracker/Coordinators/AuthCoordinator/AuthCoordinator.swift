f4044dfe208cf8c90f1154dc8c782f8cc22dfd23
Karina <barakhaeva@inbox.ru>
2025-04-06 22:24:05 +0300

    Добавлена директория для авторизации

--- ExpenseTracker/Coordinators/AuthCoordinator/AuthCoordinator.swift
+++ ExpenseTracker/Coordinators/AuthCoordinator/AuthCoordinator.swift
@@ -2,27 +2,33 @@ import UIKit
 
 final class AuthCoordinator: Coordinator, AuthCoordinatorProtocol {
     
-    var childCoordinators: [Coordinator] = []
+    // MARK: - Properties
+
+    var childCoordinators: [Coordinator] = [] // Массив для хранения дочерних координаторов
     var navigationController: UINavigationController
     weak var delegate: AuthCoordinatorDelegate?
+    
+    // MARK: - Initialization
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
+    // MARK: - Coordinator Methods
+    
+    /// Основной метод запуска координатора
     func start() {
         let authViewModel = AuthViewModel()
         let authViewController = AuthViewController(viewModel: authViewModel)
         authViewController.coordinator = self
+        
+        // Устанавливаем ViewController как корневой в навигационном контроллере
         navigationController.setViewControllers([authViewController], animated: true)
     }
     
-    func showRegistration() {
-       //
-    }
+    // MARK: - Auth Flow Methods
     
     func completeAuth() {
-        print("kjhgf")
         delegate?.didFinishAuth()
     }
 }
