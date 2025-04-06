d1baf7535081e8d681bff827f60dd64fccfa019d
Karina <barakhaeva@inbox.ru>
2025-04-06 22:35:45 +0300

    Установлено свойство координатора

--- ExpenseTracker/Modules/Auth/View/AuthViewController.swift
+++ ExpenseTracker/Modules/Auth/View/AuthViewController.swift
@@ -1,11 +1,18 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class AuthViewController: UIViewController {
     
-    var images: [String]?
-
+    weak var coordinator: AuthCoordinator?
+    
+    init(viewModel: AuthViewModel) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
     }
 }
