2252f97d9986ccee3bb134ef755c1be7054d917f
Karina <barakhaeva@inbox.ru>
2025-04-07 22:31:12 +0300

    Изменен статус авторизации на false для показа экрана авторизации

--- ExpenseTracker/Application/SceneDelegate.swift
+++ ExpenseTracker/Application/SceneDelegate.swift
@@ -8,7 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
      
-        UserDefaults.isLoggedIn = true
+        UserDefaults.isLoggedIn = false
         
         let window = UIWindow(windowScene: windowScene)
         self.window = window
