404f64b0733bfd7e495a12f9226d98d85118b685
Karina <barakhaeva@inbox.ru>
2025-04-08 19:52:23 +0300

    Добавлен модификатор final

--- ExpenseTracker/Utilities/Helpers/MainButton.swift
+++ ExpenseTracker/Utilities/Helpers/MainButton.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class MainButton: UIButton {
+final class MainButton: UIButton {
     
     init(title: String,
          backgroundColor: UIColor = .etAccent,
@@ -13,14 +13,14 @@ class MainButton: UIButton {
         
         super.init(frame: .zero)
         
-        self.setTitle(title, for: .normal)
+        setTitle(title, for: .normal)
         self.backgroundColor = backgroundColor
-        self.setTitleColor(titleColor, for: .normal)
-        self.layer.cornerRadius = cornerRadius
-        self.titleLabel?.applyTextStyle(style, textStyle: textStyle)
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.titleLabel?.numberOfLines = 1
-        self.titleLabel?.textAlignment = .center
+        setTitleColor(titleColor, for: .normal)
+        layer.cornerRadius = cornerRadius
+        titleLabel?.applyTextStyle(style, textStyle: textStyle)
+        translatesAutoresizingMaskIntoConstraints = false
+        titleLabel?.numberOfLines = 1
+        titleLabel?.textAlignment = .center
     }
     
     required init?(coder: NSCoder) {
