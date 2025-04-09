5c91bc8bcb710c304b953f4367542716ccd60fae
Karina <barakhaeva@inbox.ru>
2025-04-09 16:16:45 +0300

    [ET-19] - добавлен enum ButtonAction для состояния кнопок

---
+++ ExpenseTracker/Utilities/Helpers/ButtonAction.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum ButtonAction: String {
+    
+    // Auth
+    case login = "Войти"
+    case register = "Зарегистрироваться"
+    case createAccount = "Создайте аккаунт"
+    case forgotPassword = "Забыли пароль?"
+    case backToRegistration = "Вернуться к регистрации"
+    
+    // General
+    case send = "Отправить"
+    case confirm = "Подтвердить"
+}
