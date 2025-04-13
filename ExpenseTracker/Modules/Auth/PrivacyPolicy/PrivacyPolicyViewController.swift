bc0c195c3b5ae30dfe2dc6b4d3e6661a4d3f6a33
Karina <barakhaeva@inbox.ru>
2025-04-14 01:17:53 +0300

    [ET-2] - Создан класс для экрана политики конфиденциальности

---
+++ ExpenseTracker/Modules/Auth/PrivacyPolicy/PrivacyPolicyViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class PrivacyPolicyViewController: UIViewController {
+    
+    weak var coordinator: AuthCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .etBackground
+    }
+}
