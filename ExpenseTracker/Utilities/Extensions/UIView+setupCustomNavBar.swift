731153eb1c4b36ce6e00b5ae8b19cf95bc5726e0
Karina <barakhaeva@inbox.ru>
2025-04-15 20:04:41 +0300

    [ET-2] - Добавлено расширение для контроллеров для быстрого создания верстки кастомной навигации

---
+++ ExpenseTracker/Utilities/Extensions/UIView+setupCustomNavBar.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+extension UIViewController {
+    
+    // Создание кастомного навигационного бара
+    func setupCustomNavBar(
+        title: AuthAction,
+        isPolicyPrivacyFlow: Bool = false,
+        backAction: Selector
+    ) -> CustomBackBarItem {
+        let navBar = CustomBackBarItem(
+            title: title.rawValue,
+            isPolicyPrivacyFlow: isPolicyPrivacyFlow,
+            target: self,
+            action: backAction
+        )
+        view.addSubview(navBar)
+
+        NSLayoutConstraint.activate([
+            navBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            navBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        return navBar
+    }
+}
