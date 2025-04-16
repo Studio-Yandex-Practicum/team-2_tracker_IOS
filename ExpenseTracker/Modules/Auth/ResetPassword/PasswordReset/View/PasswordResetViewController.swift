ef137e4fef478b1972cb4f4fb9509fff2332b541
Karina <barakhaeva@inbox.ru>
2025-04-16 10:47:49 +0300

    [ET-20] - Базовая реализация класса для дальнейшей верстки

---
+++ ExpenseTracker/Modules/Auth/ResetPassword/PasswordReset/View/PasswordResetViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class PasswordResetViewController: UIViewController {
+    
+    weak var coordinator: AuthCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
