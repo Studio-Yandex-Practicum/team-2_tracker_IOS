798e00477f617d13a7773d0ced1b40133e4f62bb
Karina <barakhaeva@inbox.ru>
2025-04-16 10:48:24 +0300

    [ET-20] - Сверстан экран восстановления пароля

---
+++ ExpenseTracker/Modules/Auth/ResetPassword/PasswordRecovery/View/PasswordRecoveryViewController.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+final class PasswordRecoveryViewController: UIViewController {
+    
+    weak var coordinator: AuthCoordinator?
+    private var customNavigationBar: CustomBackBarItem?
+    
+    private let emailTextField: AuthTextField = {
+        let textField = AuthTextField(placeholder: AuthAction.mail.rawValue)
+        return textField
+    }()
+    
+    private let sendButton: MainButton = {
+        let button = MainButton(title: ButtonAction.send.rawValue, backgroundColor: .etInactive)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .etBackground
+        
+        setupNavBar()
+        setupViews()
+        setupEmailTextField()
+        setupSendButton()
+        setupTapGesture()
+    }
+    
+    private func setupNavBar() {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        customNavigationBar = setupCustomNavBar(title: .passwordRecovery, backAction: #selector(showAuthFlow))
+    }
+    
+    private func setupViews() {
+        [emailTextField, sendButton].forEach {
+            view.addSubview($0)
+            $0.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        }
+    }
+    
+    private func setupEmailTextField() {
+        guard let customNavigationBar else { return }
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(equalTo: customNavigationBar.bottomAnchor, constant: 16),
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func setupSendButton() {
+        NSLayoutConstraint.activate([
+            sendButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -31),
+            sendButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            sendButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func setupTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc
+    private func showAuthFlow() {
+        coordinator?.dismissCurrentFlow()
+    }
+}
