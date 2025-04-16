d4ff8b76a24b73bb464bfbdd47f7be8a8bc716a4
Karina <barakhaeva@inbox.ru>
2025-04-16 23:55:45 +0300

    [ET-24] - Проинициализирован RegistrationViewController

--- ExpenseTracker/Coordinators/AppCoordinator/AppCoordinator.swift
+++ ExpenseTracker/Coordinators/AppCoordinator/AppCoordinator.swift
@@ -32,6 +32,7 @@ final class AppCoordinator: Coordinator {
         let mainCoordinator = MainTabBarCoordinator(tabBarController: tabBarController)
         addChild(mainCoordinator)
         mainCoordinator.start()
+        tabBarController.selectedIndex = 1 // По умолчанию сперва будет показан экран расходов
     }
     
     // Показывает поток авторизации
